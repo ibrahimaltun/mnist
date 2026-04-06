e18a906c353a6e65e6d26efcf9c0801b8e548b19
ibrahim <bykzist34@hotmail.com>
2026-04-06 13:30:20 +0300

    add autoencoder model and model results

--- autoencoder_implementation.py
+++ autoencoder_implementation.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 import keras
@@ -25,3 +26,65 @@ x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 
 print("normalized train data shape", x_train.shape)
 print("normalized test data shape", x_test.shape)
+
+# # 2. AUTOENCODER MODEL
+enconding_dim = 32  # represents the size of encoded
+
+input_img = keras.Input(shape=(784,))  # input image as array like 28x28
+
+# "encoded" is the encoded representation of the input
+encoded = layers.Dense(enconding_dim, activation='relu')(input_img)
+
+# 'decoded' is the lossy reconstruction of the input
+decoded = layers.Dense(784, activation='sigmoid')(encoded)
+
+# This model maps an input to its reconstruction
+autoencoder = keras.Model(input_img, decoded)
+
+# This model maps as input its encoded representation
+encoder = keras.Model(input_img, encoded)
+
+# Decoder Model
+# This is our encoded (32-dimensional) input
+encoded_input = keras.Input(shape=(enconding_dim,))
+
+# Retrieve the last layer of the autoencoder model
+decoder_layer = autoencoder.layers[-1]
+
+# Create the decoder model
+decoder = keras.Model(encoded_input, decoder_layer(encoded_input))
+
+# Compile the model
+autoencoder.compile(optimizer="adam", loss="binary_crossentropy")
+
+# # 3. TRAIN AUTOENCODER
+
+autoencoder.fit(
+    x_train, x_train,
+    epochs=50,
+    batch_size=256,
+    shuffle=True,
+    # verbose=1,
+    validation_data=(x_test, x_test)
+)
+
+encoded_imgs = encoder.predict(x_test)
+decoded_imgs = decoder.predict(encoded_imgs)
+
+n = 10  # How many digits we will display
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # Display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # Display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
